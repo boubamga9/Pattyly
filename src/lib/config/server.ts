44633b6e853b81d7cf5686329fc0978c97c58bb8
bouba maiga <bouba.mga9@gmail.com>
2025-10-14 14:22:01 +0200

    small changes in /config/server.ts
    delete "or" (||)

--- src/lib/config/server.ts
+++ src/lib/config/server.ts
@@ -9,15 +9,15 @@ import { env } from '$env/dynamic/private';
  * Stripe Product IDs for different plans
  */
 export const STRIPE_PRODUCTS = {
-    BASIC: env.STRIPE_BASIC_PRODUCT_ID || 'prod_Selbd3Ne2plHqG',
-    PREMIUM: env.STRIPE_PREMIUM_PRODUCT_ID || 'prod_Selcz36pAfV3vV'
+    BASIC: env.STRIPE_BASIC_PRODUCT_ID,
+    PREMIUM: env.STRIPE_PREMIUM_PRODUCT_ID
 } as const;
 
 /**
  * Stripe Price IDs for different plans
  */
 export const STRIPE_PRICES = {
-    BASIC: env.STRIPE_BASIC_PRICE_ID || 'price_1Rre1ZPNddYt1P7Lea1N7Cbq',
-    PREMIUM: env.STRIPE_PREMIUM_PRICE_ID || 'price_1RrdwvPNddYt1P7LGICY3by5',
-    EARLY: env.STRIPE_EARLY_ADOPTERS_PRICE_ID || 'price_1SI3TVPNddYt1P7LwMYNlvXP'
+    BASIC: env.STRIPE_BASIC_PRICE_ID,
+    PREMIUM: env.STRIPE_PREMIUM_PRICE_ID,
+    EARLY: env.STRIPE_EARLY_ADOPTERS_PRICE_ID
 } as const;
