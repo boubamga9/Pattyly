4689392183cb19a3275f01f224ad5222928599b5
bouba maiga <bouba.mga9@gmail.com>
2025-11-22 11:11:43 +0100

    fix: ajouter fonction load dans layout marketing pour layout search

--- src/routes/(marketing)/+layout.server.ts
+++ src/routes/(marketing)/+layout.server.ts
@@ -1,1 +1,10 @@
+import type { LayoutServerLoad } from './$types';
+
 export const ssr = true;
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+	const { session } = await locals.safeGetSession();
+	return {
+		user: session?.user || null
+	};
+};
